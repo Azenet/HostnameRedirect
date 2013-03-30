fc61f9b4109f9785abe6407d016837e3c86159de
Azenet <shellgratuit@gmail.com>
2013-03-30 19:01:49 +0100

    Parce que en fait H2 c'est vraiment de la merde pour ça.

--- src/me/azenet/HR/HostnameRedirect.java
+++ src/me/azenet/HR/HostnameRedirect.java
@@ -1,16 +1,16 @@
 package me.azenet.HR;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,50 +18,45 @@ public class HostnameRedirect extends JavaPlugin {
 	private Logger l = Bukkit.getLogger();
 	private Connection db;
 	private Statement dbStatement;
-	private HashMap<String, HashMap<String, Object>> locations; 
-	private HashMap<String, HashMap<String, Object>> onlinePlayers;
+	private HashMap<String, HostnameRedirectLocation> locations;
+	private File locationsFile = new File(this.getDataFolder() + File.separator + "locations.ser");
 	
 	public void onEnable() {
-		locations = new HashMap<String, HashMap<String, Object>>();
-		try {
-			Class.forName("org.h2.Driver");
-			db = DriverManager.getConnection("jdbc:h2:"+this.getDataFolder().getAbsolutePath()+File.separator+"db.h2");
-			dbStatement = db.createStatement();
-			ResultSet rs = db.getMetaData().getTables(db.getCatalog(), null, null, null);
-			Boolean tableExists = false;
-			while (rs.next()) {
-				if (rs.getString("TABLE_NAME") == "points") tableExists = true;
-			}
-			if (!tableExists) {
-				dbStatement.execute("CREATE TABLE points(name varchar(255) primary key, hostname varchar(255), worldname varchar(255), x int, y int, z int, tpAnyway int)");
-			}
-			this.readDB();
-		} catch (Exception e) {
-			l.severe("You need H2 for this to run.");
-			e.printStackTrace();
-			Bukkit.getPluginManager().disablePlugin(this);
+		locations = new HashMap<String, HostnameRedirectLocation>();
+		if (!locationsFile.exists()) {
+			writeData();
+		} else {
+			locations = getDataFromLocationsFile();
 		}
 	}
 	
-	public void readDB() {
+	public void writeData() {
 		try {
-			ResultSet rs = dbStatement.executeQuery("SELECT * FROM points");
-			while (rs.next()) {
-				World w = Bukkit.getServer().getWorld(rs.getString("worldname"));
-				if (w != null) {
-					Location l = new Location(w, rs.getInt("x"), rs.getInt("y"), rs.getInt("z"));
-					HashMap<String, Object> toPut = new HashMap<String, Object>();
-					toPut.put("location", l);
-					toPut.put("tpAnyway", (Integer.valueOf(rs.getString("tpAnyway")) > 0) ? true : false);
-					toPut.put("name", rs.getString("name"));
-					locations.put(rs.getString("hostname"), toPut);
-				} else {
-					l.warning("World "+rs.getString("worldname")+" does not exist. Please delete record "+rs.getString("name")+" using /hr delete "+rs.getString("name"));
-				}
-			}
+			FileOutputStream fos = new FileOutputStream(locationsFile);
+			ObjectOutputStream out = new ObjectOutputStream(fos);
+			out.writeObject(locations);
+			out.close();
+			fos.close();
 		} catch (Exception e) {
+			l.severe("Can't write the Locations file. Your changes won't be saved.");
+			e.printStackTrace();
+		}		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public HashMap<String, HostnameRedirectLocation> getDataFromLocationsFile() {
+		try {
+			FileInputStream fis = new FileInputStream(locationsFile);
+			ObjectInputStream in = new ObjectInputStream(fis);
+			HashMap<String, HostnameRedirectLocation> toReturn = (HashMap<String, HostnameRedirectLocation>) in.readObject();
+			in.close();
+			fis.close();
+			return toReturn;
+		} catch (Exception e) {
+			l.severe("Can't read the locations file. Please delete locations.ser");
 			e.printStackTrace();
 		}
+		return null;
 	}
 	
 	public void playerOnline(Player p) {
@@ -75,15 +70,16 @@ public class HostnameRedirect extends JavaPlugin {
 	}
 	
 	public Boolean isLocation(String locationName) {
-		for (HashMap<String, Object> hm : locations.values()) {
-			if (((String)hm.get("name")).equalsIgnoreCase(locationName)) return true;
-		}
-		return false;
+		return locations.containsKey(locationName);
 	}
 	
-	public HashMap<String, Object> getLocationFromName(String locationName) {
-		for (HashMap<String, Object> hm : locations.values()) {
-			if (((String)hm.get("name")).equalsIgnoreCase(locationName)) return hm;
+	public HostnameRedirectLocation getLocationFromName(String locationName) {
+		return (isLocation(locationName)) ? locations.get(locationName) : null;
+	}
+	
+	public HostnameRedirectLocation getLocationFromHostname(String hostname) {
+		for (HostnameRedirectLocation hrl : locations.values()) {
+			if (hrl.getHostname() == hostname) return hrl;
 		}
 		return null;
 	}
---
+++ src/me/azenet/HR/HostnameRedirectLocation.java
@@ -0,0 +1,57 @@
+package me.azenet.HR;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public class HostnameRedirectLocation {
+	private Double x;
+	private Double y;
+	private Double z;
+	private Float yaw;
+	private Float pitch;
+	private String hostname;
+	private String name;
+	private Boolean tpAnyway;
+	private World world;
+
+	public HostnameRedirectLocation(String name, String hostname, World world, double x, double y, double z) {
+		this(name, hostname, world, x, y, z, false, 0F, 0F);
+	}
+	public HostnameRedirectLocation(String name, String hostname, World world, double x, double y, double z, boolean tpAnyway) {
+		this(name, hostname, world, x, y, z, tpAnyway, 0F, 0F);
+	}
+	public HostnameRedirectLocation(String name, String hostname, Location loc) {
+		this(name, hostname, loc.getWorld(), loc.getX(), loc.getY(), loc.getZ(), false, loc.getYaw(), loc.getPitch());
+	}
+	public HostnameRedirectLocation(String name, String hostname, Location loc, Boolean tpAnyway) {
+		this(name, hostname, loc.getWorld(), loc.getX(), loc.getY(), loc.getZ(), tpAnyway, loc.getYaw(), loc.getPitch());
+	}
+	public HostnameRedirectLocation(String name, String hostname, World world, double x, double y, double z, boolean tpAnyway, float yaw, float pitch) {
+		this.name = name;
+		this.hostname = hostname;
+		this.world = world;
+		this.x = Double.valueOf(x);
+		this.y = Double.valueOf(y);
+		this.z = Double.valueOf(z);
+		this.tpAnyway = tpAnyway;
+		this.yaw = Float.valueOf(yaw);
+		this.pitch = Float.valueOf(pitch);
+	}
+	
+	public Location getLocation() {
+		return new Location(world, x, y, z, yaw, pitch);
+	}
+	
+	public String getHostname() {
+		return hostname;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Boolean hasToTP() {
+		return tpAnyway;
+	}
+	
+}
