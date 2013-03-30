4e0a2cebde6a09fb1016b384725a810537727cc1
Azenet <shellgratuit@gmail.com>
2013-03-30 16:10:53 +0100

    initial commit

---
+++ src/me/azenet/HR/HostnameRedirect.java
@@ -0,0 +1,14 @@
+package me.azenet.HR;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HostnameRedirect extends JavaPlugin {
+
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
